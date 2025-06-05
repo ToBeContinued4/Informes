2fba3dda9b9a43319a59a1a487190fce3e747aaf
Tobe <117567068+ToBeContinued4@users.noreply.github.com>
2025-06-06 00:47:26 +0200

    Add files via upload

--- src/informes.cpp
+++ src/informes.cpp
@@ -4,60 +4,11 @@
 
 using namespace std;
 
-string ruta;
-
-void selection();
-void creation();
-void creation_advanced();
-
 int main() {
-    int input_main;
-    
-    system("cls");
-    system("title Informes v0.2.0 - test build");
-
-    cout << "Informes v0.2.0 - test build" << endl << "--------------------" << endl;
-    cout << "1. Hacer informe" << endl << "2. Salir" << endl << endl;
-    cout << "> ";
-    cin >> input_main;
-
-    if (input_main == 1) {
-        selection();
-    } else if (input_main == 2) {
-        return 0;
-    } else {
-        cout << "Opcion invalida" << endl;
-        system("pause");
-        main();
-    }
-}
-
-void selection() {
-    int input_selection;
-
-    system("cls");
-
-    cout << "Selecciona el tipo de informe" << endl << "--------------------" << endl;
-    cout << "1. Informe Basico" << endl << "2. Informe Avanzado" << endl << "3. Volver" << endl << "--------------------" << endl << endl;
-    cout << "> ";
-    cin >> input_selection;
-
-    if (input_selection == 1) {
-        creation();
-    } else if (input_selection == 2) {
-        creation_advanced();
-    } else if (input_selection == 3) {
-        main();
-    }
-}
-
-void creation() {
     int cantidad;
     string tipo, estado, rendimiento, observacion;
 
-    system("cls");
-
-    cout << "Informe Basico" << endl << "--------------------" << endl << endl;
+    system("title Informes v0.1.3");
 
     cout << "Cantidad de dispositivos: ";
     cin >> cantidad;
@@ -66,7 +17,7 @@ void creation() {
     if (cantidad < 1) {
         cout << "El informe tiene que ser para al menos 1 dispositivo." << endl;
         system("pause");
-        main();
+        return 1;
     } else if (cantidad > 1) {
         cout << "Tipo de los dispositivos: ";
         cin >> tipo;
@@ -114,97 +65,7 @@ void creation() {
     }
 
     file.close();
-
-    system("cls");
     cout << "Informe completado correctamente!" << endl;
-
     system("pause");
-    main();
-}
-
-void creation_advanced() {
-    int cantidad;
-    string tipo, estado, rendimiento, observacion, serial, id;
-
-    system("cls");
-
-    cout << "Informe Avanzado" << endl << "--------------------" << endl << endl;
-
-    cout << "Cantidad de dispositivos: ";
-    cin >> cantidad;
-    cin.ignore();
-
-    if (cantidad < 1) {
-        cout << "El informe tiene que ser para al menos 1 dispositivo." << endl;
-        system("pause");
-        main();
-    } else if (cantidad > 1) {
-        cout << "Tipo de los dispositivos: ";
-        cin >> tipo;
-        cin.ignore(100, '\n');
-
-        tipo[0] = toupper(tipo[0]);
-    } else {
-        cout << "Tipo del dispositivo: ";
-        cin >> tipo;
-        cin.ignore(100, '\n');
-
-        tipo[0] = toupper(tipo[0]);
-    }
-
-    cout << endl;
-
-    ofstream file("Informe" + tipo + ".txt");
-    file << "Informe para " << cantidad << " " << tipo << endl << "-----------------------" << endl;
-
-    for (int i = 1; i <= cantidad; i++) {
-        file << endl << tipo << " " << i << endl << "------------" << endl;
-
-        cout << "ID del dispositivo " << i << ": ";
-        getline(cin, id);
-
-        cout << "Numero de serie del dispositivo " << i << ": ";
-        getline(cin, serial);
-
-        cout << "Estado fisico del dispositivo " << i << ": ";
-        getline(cin, estado);
-
-        cout << "Rendimiento del dispositivo " << i << ": ";
-        getline(cin, rendimiento);
-
-        cout << "Observacion del dispositivo " << i << ": ";
-        getline(cin, observacion);
-
-        cout << endl;
-
-        if (!id.empty()) {
-            file << "ID: " << id << endl;
-        }
-
-        if (!serial.empty()) {
-            file << "Numero de serie: " << serial << endl;
-        }
-
-        file << "------------" << endl;
-
-        if (!estado.empty()) {
-            file << "Estado fisico: " << estado << endl;
-        }
-        
-        if (!rendimiento.empty()) {
-            file << "Rendimiento: " << rendimiento << endl << endl;
-        }
-        
-        if (!observacion.empty()) {
-            file << observacion << endl << endl;
-        }
-    }
-
-    file.close();
-
-    system("cls");
-    cout << "Informe completado correctamente!" << endl;
-
-    system("pause");
-    main();
+    return 0;
 }
